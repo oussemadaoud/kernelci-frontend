5cf23653701ea878b0875f642dfd87e4f4d6cb62
Oussema DAOUD <odaoud@baylibre.com>
2018-09-07 18:08:02 +0200

    Tests by build: kernel, board and test set
    Create the tests by build for selected kernel, selected board and selected test set view
    
    Signed-off-by: Oussema DAOUD <odaoud@baylibre.com>

--- app/dashboard/static/js/common.js
+++ app/dashboard/static/js/common.js
@@ -86,7 +86,8 @@ require.config({
             'app/view-releases-all': 'app/view-releases-all.2018.07.30',
             'app/view-releases-kernel': 'app/view-releases-kernel.2018.07.30',
             'app/view-releases-kernel-board': 'app/view-releases-kernel-board.2018.07.30',
-            'app/view-releases-kernel-board-suite-name': 'app/view-releases-kernel-board-suite-name.2018.07.30'
+            'app/view-releases-kernel-board-suite-name': 'app/view-releases-kernel-board-suite-name.2018.07.30',
+            'app/view-releases-kernel-board-set-name': 'app/view-releases-kernel-board-set-name.2018.07.30'
         }
     },
     shim: {
